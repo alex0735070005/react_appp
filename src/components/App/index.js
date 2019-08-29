ae941e1d9b152725097252575a6a731ab0b34a96
alex0735070005 <alex0735070005@gmail.com>
2019-08-29 20:32:50 +0300

    v2

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './Header';
+import Header from '../Header';
+import './style.scss';
 
 function App() {
   return (
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+function Header () {
+  return (
+    <div id="header">
+      <span>Hello Header</span>
+    </div>
+  )
+}
+
+export default Header;
